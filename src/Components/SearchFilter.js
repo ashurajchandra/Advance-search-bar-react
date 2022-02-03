3d87379c9c1f56b17dce99b565c65f07f7033191
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2022-02-03 15:57:04 +0530

    showing filtered data on typing

--- src/Components/SearchFilter.js
+++ src/Components/SearchFilter.js
@@ -4,28 +4,47 @@ import CloseIcon from '@mui/icons-material/Close';
 import './SearchFilter.css';
 
  function SearchFilter({placeholder,data}) {
+    const [filteredData, setFilteredData] = useState([]);
      const [enterWord, setenterWord] = useState('');
+
      console.log("placeholder",placeholder);
      console.log("data",data)
+
+     const handleFilteredData=(e)=>{
+         const searchWord= e.target.value;
+         setenterWord(searchWord)
+
+         const newFilteredData = data.filter(value=>{
+             return value.title.toLowerCase().includes(searchWord.toLowerCase())
+         })
+         if(enterWord !=''){
+            setFilteredData(newFilteredData )
+         }
+         else{
+            setFilteredData([])
+         }
+         
+     }
     const handleClearInputField= ()=>{
+        setFilteredData([]);
         setenterWord('')
      }
   return(  <div className="search">
     <div className="searchInputs">
-        <input type="text"placeholder={placeholder} value={enterWord} onChange={e=>setenterWord(e.target.value)}/>
+        <input type="text"placeholder={placeholder} value={enterWord} onChange={handleFilteredData}/>
         <div className='searchIcon'>
             {enterWord ===''?
                 <SearchIcon />
                 :<CloseIcon id="clearBtn" onClick={handleClearInputField}  />}
             </div>
             </div>
-            <div className='dataResult'>
-                {data.map((data)=>{
+          {enterWord.length>0 &&   <div className='dataResult'>
+                {filteredData.slice(0,15).map((data)=>{
                     return(
                         <p><a className='dataItem' target="_blank" href={data.link}>{data.title}</a></p>
                     )
                 })}
-            </div>
+            </div>}
   </div>);
 }
 export default SearchFilter;
